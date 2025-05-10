a059329be4742a9a0f60db09ee5554b6c9987006
Raphael Peters <rappet@rappet.de>
2025-05-10 12:25:26 +0200

    tests: Add skeletton for CLI tests

---
+++ tests/cli.rs
@@ -0,0 +1,16 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+const CMD_NAME: &str = "tlslb";
+
+#[test]
+fn fails_without_args() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(CMD_NAME)?;
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("the following required arguments were not provided:"));
+
+    Ok(())
+}
