ff362db529ef2ccce52381da540de065770ae6ae
Raphael Peters <rappet@rappet.de>
2025-06-01 17:12:53 +0200

    ip-database: Implement IP -> ASN lookup

--- ip-database/src/ip.rs
+++ ip-database/src/ip.rs
@@ -1,8 +1,11 @@
 use std::{
-    io::{Read, Write},
+    io::{BufRead, BufReader, Read, Write},
     marker::PhantomData,
     net::IpAddr,
 };
+
+use ipnet::{IpNet, Ipv4Net, Ipv6Net};
+use prefix_trie::PrefixMap;
 
 use crate::LoadIpDatabaseError;
 
@@ -30,11 +33,12 @@ use crate::LoadIpDatabaseError;
 ///  // Normally, you would keep this in some shared state
 ///  let db = Arc::new(db);
 ///
-///  let ip_ref = db.lookup_ip(IpAddr::V4(Ipv4Addr::new(9, 9, 9, 9))).expect("entry exists in example data");
-///  assert_eq!(ip_ref.asn(), 19281);
+///  let ip_ref = db.lookup_ip(IpAddr::V4(Ipv4Addr::new(203, 0, 113, 23))).expect("entry exists in example data");
+///  assert_eq!(ip_ref.asn(), 64497);
 /// ```
 pub struct IpDatabase {
-    // TBD....
+    ipv4_to_asn: PrefixMap<Ipv4Net, u32>,
+    ipv6_to_asn: PrefixMap<Ipv6Net, u32>,
 }
 
 impl IpDatabase {
@@ -45,10 +49,16 @@ impl IpDatabase {
     /// # TODO
     /// Maybe we want to bind this to the AS database?
     pub fn new() -> Self {
-        IpDatabase {}
+        IpDatabase {
+            ipv4_to_asn: PrefixMap::new(),
+            ipv6_to_asn: PrefixMap::new(),
+        }
     }
 
     /// Lookup metadata of an IP address
+    ///
+    /// This function supports IPv4-compatible and IPv4-mapped IP addresses and interprets
+    /// them as IPv4 addresses.
     ///
     /// # Example
     ///
@@ -57,11 +67,26 @@ impl IpDatabase {
     /// # use ip_database::IpDatabase;
     /// # let mut db = IpDatabase::new();
     /// # db.load_routing_table_txt(&include_bytes!("./test-route-table.txt")[..]).expect("test data works");
-    /// let ip_ref = db.lookup_ip(IpAddr::V4(Ipv4Addr::new(9, 9, 9, 9))).expect("entry exists in example data");
-    /// assert_eq!(ip_ref.asn(), 19281);
+    /// let ip_ref = db.lookup_ip(IpAddr::V4(Ipv4Addr::new(203, 0, 113, 23))).expect("entry exists in example data");
+    /// assert_eq!(ip_ref.asn(), 64497);
     /// ```
     pub fn lookup_ip(&self, addr: IpAddr) -> Option<IpRef> {
-        todo!()
+        // we throw away the IP-network here as this will be aggregated and might not correspond
+        // to an actual BGP-route in the DFZ anymore
+        let asn = match normalize_ip_address(addr) {
+            IpAddr::V4(ipv4) => self
+                .ipv4_to_asn
+                .get_lpm(&Ipv4Net::from(ipv4))
+                .map(|(_net, &addr)| addr),
+            IpAddr::V6(ipv6) => self
+                .ipv6_to_asn
+                .get_lpm(&Ipv6Net::from(ipv6))
+                .map(|(_net, &addr)| addr),
+        };
+        asn.map(|asn| IpRef {
+            asn: asn,
+            _dummy: Default::default(),
+        })
     }
 
     /// Load an IP route to AS mapping from a txt stream
@@ -84,11 +109,32 @@ impl IpDatabase {
     /// let mut db = IpDatabase::new();
     /// db.load_routing_table_txt(reader).expect("the data from the reader is correct");
     /// ```
-    pub fn load_routing_table_txt(
-        &mut self,
-        mut reader: impl Read,
-    ) -> Result<Self, LoadIpDatabaseError> {
-        todo!()
+    pub fn load_routing_table_txt(&mut self, reader: impl Read) -> Result<(), LoadIpDatabaseError> {
+        for line_res in BufReader::new(reader).lines() {
+            let line = line_res?;
+            if line.trim().is_empty() || line.trim_start().starts_with('#') {
+                continue;
+            }
+            let (route_raw, asn_raw) = line.split_once(' ').ok_or_else(|| {
+                LoadIpDatabaseError::MalformedInputFile("line is not split by route and ASN".into())
+            })?;
+            let net: IpNet = route_raw.parse().map_err(|_err| {
+                LoadIpDatabaseError::MalformedInputFile(format!(
+                    "ip network {route_raw:?} is malformed in line {line:?}"
+                ))
+            })?;
+            let asn: u32 = asn_raw.parse().map_err(|_err| {
+                LoadIpDatabaseError::MalformedInputFile(format!(
+                    "asn {asn_raw:?} is malformed in line {line:?}"
+                ))
+            })?;
+            match net {
+                IpNet::V4(net_v4) => self.ipv4_to_asn.insert(Ipv4Net::from(net_v4), asn),
+                IpNet::V6(net_v6) => self.ipv6_to_asn.insert(Ipv6Net::from(net_v6), asn),
+            };
+        }
+
+        Ok(())
     }
 
     /// Store the IP route to ASN mapping to a file
@@ -99,6 +145,21 @@ impl IpDatabase {
         mut writer: impl Write,
     ) -> Result<(), LoadIpDatabaseError> {
         todo!()
+    }
+}
+
+fn normalize_ip_address(ip_addr: IpAddr) -> IpAddr {
+    match ip_addr {
+        IpAddr::V4(ip_addr) => IpAddr::V4(ip_addr),
+        IpAddr::V6(ip_addr) => {
+            if let Some(ipv4) = ip_addr.to_ipv4() {
+                IpAddr::V4(ipv4)
+            } else if let Some(ipv4) = ip_addr.to_ipv4_mapped() {
+                IpAddr::V4(ipv4)
+            } else {
+                IpAddr::V6(ip_addr)
+            }
+        }
     }
 }
 
--- ip-database/src/lib.rs
+++ ip-database/src/lib.rs
@@ -1,8 +1,6 @@
 // mod asn;
 mod ip;
 
-use std::io::{Read, Write};
-
 // pub use asn::{AsDatabase, AsRef};
 pub use ip::{IpDatabase, IpRef};
 use thiserror::Error;
@@ -10,9 +8,9 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum LoadIpDatabaseError {
     #[error(transparent)]
-    Io(std::io::Error),
-    #[error("database is malformed")]
-    MalformedDatabase(String),
+    Io(#[from] std::io::Error),
+    #[error("input file is malformed: {0}")]
+    MalformedInputFile(String),
     #[error("forbidden to fetch source data")]
     Forbidden,
 }
